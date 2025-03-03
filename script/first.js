9c7bde7765deca14d0aafdc0f7b705e77d41deb6
Your Name <rishadahammed209@gmail.com>
2025-03-03 23:33:42 +0600

    onk code push korsi r prmo nah

--- script/first.js
+++ script/first.js
@@ -1,20 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     for (let i = 1; i <= 6; i++) {
-//         const completeButton = document.getElementById(`completeButton${i}`);
-//         if (completeButton) {
-//             completeButton.addEventListener('click', function() {
-//                 alert(`Task ${i} completed!`);
-//                 completeButton.disabled = true;
-//             });
-//         }
-//     }
-// });
-
-
-
-
-
 document.getElementById('complate-btn-1').addEventListener('click', function () {
 
 
@@ -26,26 +10,15 @@ document.getElementById('complate-btn-1').addEventListener('click', function () 
 
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-1');
         classAdd.setAttribute('disabled', true)
     }
-
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-    
-
-
-   
-    // time....
-
     let dates2 = new Date();
 
     const history = document.getElementById('add-history');
@@ -57,9 +30,6 @@ document.getElementById('complate-btn-1').addEventListener('click', function () 
 `
     addHistory.classList.add('extra-style')
     history.appendChild(addHistory);
-
-
-
 })
 
 document.getElementById('complate-btn-2').addEventListener('click', function () {
@@ -72,26 +42,16 @@ document.getElementById('complate-btn-2').addEventListener('click', function () 
 
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-2');
         classAdd.setAttribute('disabled', true)
     }
-
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-
-    // time....
-
     let dates2 = new Date();
-
     const history = document.getElementById('add-history');
     const title1 = document.getElementById('title-2').innerText;
     const addHistory = document.createElement('div');
@@ -103,37 +63,23 @@ document.getElementById('complate-btn-2').addEventListener('click', function () 
     history.appendChild(addHistory);
 
 })
-
 document.getElementById('complate-btn-3').addEventListener('click', function () {
-
     const task = document.getElementById('task-number').innerText;
     const taskValue = parseInt(task) - 1;
     document.getElementById('task-number').innerText = taskValue;
-
     const addTask = document.getElementById('add-task').innerText;
-
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-3');
         classAdd.setAttribute('disabled', true)
     }
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-
-
-    // time....
-
     let dates2 = new Date();
-
     const history = document.getElementById('add-history');
     const title1 = document.getElementById('title-3').innerText;
     const addHistory = document.createElement('div');
@@ -143,40 +89,24 @@ document.getElementById('complate-btn-3').addEventListener('click', function () 
 `
     addHistory.classList.add('extra-style')
     history.appendChild(addHistory);
-
 })
-
 document.getElementById('complate-btn-4').addEventListener('click', function () {
-
     const task = document.getElementById('task-number').innerText;
     const taskValue = parseInt(task) - 1;
     document.getElementById('task-number').innerText = taskValue;
-
     const addTask = document.getElementById('add-task').innerText;
-
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-4');
         classAdd.setAttribute('disabled', true)
     }
-
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-
-
-    // time....
-
     let dates2 = new Date();
-
     const history = document.getElementById('add-history');
     const title1 = document.getElementById('title-4').innerText;
     const addHistory = document.createElement('div');
@@ -185,150 +115,81 @@ document.getElementById('complate-btn-4').addEventListener('click', function () 
 
 `
     addHistory.classList.add('extra-style')
-    history.appendChild(addHistory);
-
-    
+    history.appendChild(addHistory);  
 })
-
 document.getElementById('complate-btn-5').addEventListener('click', function () {
-
     const task = document.getElementById('task-number').innerText;
     const taskValue = parseInt(task) - 1;
     document.getElementById('task-number').innerText = taskValue;
-
     const addTask = document.getElementById('add-task').innerText;
-
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-5');
         classAdd.setAttribute('disabled', true)
     }
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-
-
-    // time....
-
     let dates2 = new Date();
-
     const history = document.getElementById('add-history');
     const title1 = document.getElementById('title-5').innerText;
     const addHistory = document.createElement('div');
     addHistory.innerHTML = `
 <p>You have Complete The Task ${title1} at ${dates2.toLocaleTimeString()}</p>
-
 `
     addHistory.classList.add('extra-style')
     history.appendChild(addHistory);
-
-   
-
 })
-
 document.getElementById('complate-btn-6').addEventListener('click', function addTime() {
-
-
-
     const task = document.getElementById('task-number').innerText;
     const taskValue = parseInt(task) - 1;
     document.getElementById('task-number').innerText = taskValue;
-
     const addTask = document.getElementById('add-task').innerText;
-
     const addValue = parseInt(addTask) + 1;
     document.getElementById('add-task').innerText = addValue;
-
-
-
     alert('Board Updated Successfully')
     if (alert) {
         const classAdd = document.getElementById('complate-btn-6');
         classAdd.setAttribute('disabled', true);
     }
-
-
-
     const goodAlert = document.getElementById('task-number').innerText;
     if(goodAlert == 0){
         alert('Congrats!!! You have completed all the current task')
     }
-
-
-    // time....
-
     let dates2 = new Date();
-
     const history = document.getElementById('add-history');
     const title1 = document.getElementById('title-6').innerText;
     const addHistory = document.createElement('div');
     addHistory.innerHTML = `
 <p>You have Complete The Task ${title1} at ${dates2.toLocaleTimeString()}</p>
-
 `
     addHistory.classList.add('extra-style')
-    history.appendChild(addHistory);
-
-   
+    history.appendChild(addHistory);  
 })
-
-
-
-
-
-
-
-
-
-
-
 document.getElementById('clear-btn').addEventListener('click', function () {
     const history2 = document.getElementById('add-history');
     history2.classList.add('hidden')
-
 })
-
-
-
 document.getElementById('blog').addEventListener('click', function () {
-
-    window.location.href = './blog.html'
+    window.location.href = 'main.html'
 },)
-
-
 const dates2 = new Date();
 const months = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 const dates = dates2.getDate();
 const month = months[dates2.getMonth()];
 const year = dates2.getFullYear();
-
 const setDate = dates + " " + month + " " + year;
-
 const dayTo = days[dates2.getDay()];
 const fixedDay = dayTo.slice(0, 3);
-
 document.getElementById('day-name').innerText = fixedDay + ',';
-
 document.getElementById('date-name').innerText = setDate;
-
-
-
-
-
 document.getElementById('theme-change').addEventListener('click', function () {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
     document.body.style.backgroundColor = randomColor;
 })
 
